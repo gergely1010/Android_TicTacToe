53cd67571f953a9ccf4e61bb0b390869179dab50
Kurucz Gergely <Gergely.Kurucz@hungeritzrt.hu>
2024-04-30 10:02:59 +0200

    Kilépés gomb

--- app/src/main/java/com/example/android_tictactoe/MainActivity.java
+++ app/src/main/java/com/example/android_tictactoe/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android_tictactoe;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnQuit, btnStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnQuit = (Button) findViewById(R.id.btnQuit);
+        btnQuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
